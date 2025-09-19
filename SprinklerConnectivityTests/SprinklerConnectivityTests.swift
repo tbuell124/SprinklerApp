3dd6a87006796b4715582831c27e266677e13b8b
Ty Buell <tybuell@gmail.com>
2025-09-18 23:47:48 -0400

    update

---
+++ SprinklerConnectivityTests/SprinklerConnectivityTests.swift
@@ -0,0 +1,16 @@
+//
+//  SprinklerConnectivityTests.swift
+//  SprinklerConnectivityTests
+//
+//  Created by Tyler Buell on 9/18/25.
+//
+
+import Testing
+
+struct SprinklerConnectivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
